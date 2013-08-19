e9235a76138fd5f0812beaa018e28a3dd5632965
Felipe Ferreira <felipehfj@gmail.com>
2013-08-19 18:46:58 -0300

    Adicionada autor e data

--- Arduino/Diferencial.h
+++ Arduino/Diferencial.h
@@ -24,19 +24,53 @@ class Diferencial{
         #atributos
 		Motor _motorDireito;
 		Motor _motorEsquerdo;
+		uint8_t _velocidade;
         #metodos
 		Diferencial(Motor direito, Motor esquerdo){
-            this->_motorDireito = direita;
+            this->_motorDireito = direito;
             this->_motorEsquerdo = esquerdo;
 		};
-		~Diferencial(){};
+
+		~Diferencial(){
+            delete(this->_motorDireito);
+            delete(this->_motorEsquerdo);
+		};
 
 	public:
-		void direita(){};
-		void esquerda(){};
-		void frente(){};
-		void tras(){};
-		void parar(){};
+		void direita(){
+            this->_motorDireito.frente();
+            this->_motorEsquerdo.tras();
+		};
+
+		void esquerda(){
+            this->_motorDireito.tras();
+            this->_motorEsquerdo.frente();
+		};
+
+		void frente(){
+            this->_motorDireito.frente();
+            this->_motorEsquerdo.frente();
+		};
+
+		void tras(){
+            this->_motorDireito.tras();
+            this->_motorEsquerdo.tras();
+		};
+
+		void parar(){
+            this->_motorDireito.parar();
+            this->_motorEsquerdo.parar();
+		};
+
+		void alteraVelocidade(uint8_t velocidade){
+            uint8_t velocidadeMotorDireito = this->_motorDireito.getVelocidade();
+            uint8_t velocidadeMotorEsquerdo = this->_motorEsquerdo.getVelocidade();
+
+            if((velocidadeMotorDireito && velocidadeMotorEsquerdo) != velocidade){
+                this->_motorDireito.setVelocidade(velocidade);
+                this->_motorEsquerdo.setVelocidade(velocidade);
+            }
+		};
 
 };
 #endif
